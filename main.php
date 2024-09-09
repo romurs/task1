6e0123a5348ac0f4dd5f3b28301ed35ff4258676
romurs <1romurs@gmail.com>
2024-09-10 01:15:10 +0300

    refactor: rename variables to lowerCase

--- main.php
+++ main.php
@@ -5,21 +5,21 @@ require_once 'vendor/autoload.php';
 use Romurs\Task1\Library;
 use Romurs\Task1\Book;
 
-$Book = new Book("Фасад", "Роман", "2024", "Ужастик");
-$ABC = new Book('Азбука', 'Англия', '2003', 'Обучение');
-$Kniga = new Book('Африка', 'Роман', '1999', 'Негры');
-$Library = new Library;
+$book = new Book("Фасад", "Роман", "2024", "Ужастик");
+$abc = new Book('Азбука', 'Англия', '2003', 'Обучение');
+$kniga = new Book('Африка', 'Роман', '1999', 'Негры');
+$library = new Library;
 
 // print($Book->getBookInfo());
 
-$Library->addBook($Book);
-$Library->addBook($ABC);
-$Library->addBook($Kniga);
+$library->addBook($Book);
+$library->addBook($ABC);
+$library->addBook($Kniga);
 
-$Library->listAllBooks();
+$library->listAllBooks();
 
-$Library->findBookByAuthor('Роман');
+$library->findBookByAuthor('Роман');
 
-$Library->removeBookByTitle('Азбука');
-$Library->listAllBooks();
+$library->removeBookByTitle('Азбука');
+$library->listAllBooks();
 
